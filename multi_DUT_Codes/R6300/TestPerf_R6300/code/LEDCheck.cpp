b9355109cab9024df9e6fbe55a6d3292a709599e
zfh1005 <zfh1005@126.com>
2015-04-09 18:03:21 +0800

    change some parameter setting error
    The setting error found by LiShenZhao.

--- multi_DUT_Codes/R6300/TestPerf_R6300/code/LEDCheck.cpp
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/LEDCheck.cpp
@@ -3,10 +3,10 @@ using namespace std;
 #include "LEDCheck.h"
 
 
-#define SERIAL_PORT "/dev/ttyUSB1"
+#define SERIAL_PORT "/dev/ttyUSB0"
 #define SW_VERSION "1.0"
-#define CMMD_LENGTH 100
-#define TIME_OUT 15000  //10S
+#define CMMD_LENGTH 128
+#define TIME_OUT 15000  //15S
 
 LEDCheck::LEDCheck()
 {
