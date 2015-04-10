3244efe4ff149ebb529be4a18317aa1513822c7a
zfh1005 <zfh1005@126.com>
2015-04-10 09:07:29 +0800

    add Socket class into project
    add Socket class into project used for Socket operation.

---
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/Socket.cpp
@@ -0,0 +1,18 @@
+ // Implementation of the Socket class.
+
+
+#include "Socket.h"
+
+using namespace std;
+Socket::Socket() :
+  m_sock ( -1 )
+{
+
+  
+}
+
+Socket::~Socket()
+{
+
+}
+
---
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/Socket.h
@@ -0,0 +1,18 @@
+// Definition of the Socket class
+
+#ifndef SOCKET_H
+#define SOCKET_H
+
+class Socket
+{
+ public:
+  Socket();
+  virtual ~Socket();
+
+  
+
+
+};
+
+
+#endif
