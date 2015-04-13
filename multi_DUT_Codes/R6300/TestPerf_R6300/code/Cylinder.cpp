e5ce4c30e53a0fc9bd03a4fd5a9c0b202f3eeb12
zfh1005 <zfh1005@126.com>
2015-04-13 15:40:16 +0800

    add Cylinder's struct function
    add Cylinder's struct function

--- multi_DUT_Codes/R6300/TestPerf_R6300/code/Cylinder.cpp
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/Cylinder.cpp
@@ -1,7 +1,63 @@
 #include "Cylinder.h"
 
+#include "Cylinder.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 Cylinder::Cylinder()
 {
-	return 1;
+    if((fd_i2c=open(I2C_DEVICE_NAME,O_RDWR))<0)
+  
+    {
+    	perror("Can not open device.\n");
+    	exit(1);
+    }
+    //³õÊ¼»¯ 
+    MV_I2C_IOCTL_PARMS params;
+    int board_ioctl_signal=BOARD_IOCTL_I2C_CTRL;
+    ////Initialization for PCA9555
+    audio_status[0]=0x06;
+    audio_status[1]=0x00;
+    audio_status[2]=0x00;
+    params.action=I2C_WRITE;
+    params.chanNum=0;
+    params.offset=0;
+    params.alen=0;
+    params.len=3;
+    params.data=(char *)audio_status;
+    params.dev_addr=AUDIOSW_ADDRESS;
+    ioctl(fd_i2c,board_ioctl_signal,&params);
+    
+    audio_status[0]=0x02;
+    audio_status[1]=0xff;
+    audio_status[2]=0xff;
+    params.action=I2C_WRITE;
+    params.chanNum=0;
+    params.offset=0;
+    params.alen=0;
+    params.len=3;
+    params.data=(char *)audio_status;
+    params.dev_addr=AUDIOSW_ADDRESS;
+    ioctl(fd_i2c,board_ioctl_signal,&params);
+    
+    //Initialization for PCA8575
+    params.action=I2C_WRITE;
+    params.chanNum=0;
+    params.dev_addr=RELAY_ADDRESS;
+    params.offset=0;
+    params.alen=0;
+    relay_status[0]=0x00;
+    relay_status[1]=0xFF;
+    params.data=(char *)relay_status;
+    params.len=2;
+    
+    ioctl(fd_i2c,board_ioctl_signal,&params);
+    
+    ////Ö´ÐÐÃüÁî
+    
+
+}
+Cylinder::~Cylinder()
+{   
+     close(fd_i2c); 
 }
--- multi_DUT_Codes/R6300/TestPerf_R6300/code/Cylinder.h
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/Cylinder.h
@@ -1,16 +1,59 @@
 #ifndef CYLINDER_H
 #define CYLINDER_H
 
-
+#include <string>                                                                     
+#include <iostream>
+#include <pthread.h>
+#include <fcntl.h> 
+#include <unistd.h> 
+#include <sys/types.h>  
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdlib.h> 
+#include <stdio.h>
+#include <sys/ioctl.h>
+using namespace std;
 
 class Cylinder
 {
- 
+	#define AUDIOSW_ADDRESS 0x24  
+    #define BOARD_IOCTL_I2C_CTRL 0x1
+    #define RELAY_ADDRESS 0x24
+    #define AUDIOSW_ADDRESS 0x24 
+    unsigned char relay_status[2];
+    unsigned char audio_status[3];
+    typedef enum 
+    {
+        I2C_READ=0,
+        I2C_WRITE,
+        I2C_PROBE
+    }MV_I2C_ACTION;
+    
+    typedef struct MVI2CIoctlParams
+    {
+        MV_I2C_ACTION action;
+        char chanNum;
+        char dev_addr;
+        int offset;
+        int alen;
+        char* data;
+        int len;
+    } MV_I2C_IOCTL_PARMS;
+    
+    #define I2C_DEVICE_NAME   "/dev/i2c"
+    #define BUFFER_SIZE 5120
+    #define MAX_RELAY_PORTS 16
+    #define MAX_AUDIOSWITCH_PORTS 12
+
+public:    
+    int fd_i2c;
+  
+
 public:
     Cylinder();
 public:
     virtual ~Cylinder();
-
-     
+	
+	
 };
 #endif
