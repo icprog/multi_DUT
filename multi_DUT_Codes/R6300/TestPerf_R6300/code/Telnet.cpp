352547d232b32f98c10f65afbc0fe2e6417ccc00
zfh1005 <zfh1005@126.com>
2015-04-20 11:45:06 +0800

    complete the struct function in CTelnet
    complete the struct function in CTelnet

--- multi_DUT_Codes/R6300/TestPerf_R6300/code/Telnet.cpp
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/Telnet.cpp
@@ -1,12 +1,38 @@
 
 #include "Telnet.h"
 
+#define		N		16
+#define		PORT 	23
+
 
 CTelnet::CTelnet(char *ipAddr)
 {
+	memset(ipAddress,0,sizeof(ipAddress));
+	strcpy(ipAddress,ipAddr);
 	
+	if ((sockfd=socket(AF_INET,SOCK_STREAM,0))==-1) //set up socket	
+	{
+        	perror("socket");
+        	exit(1);
+    	} 
+	
+    	their_addr.sin_family = AF_INET;
+    	their_addr.sin_port = htons(PORT);
+        their_addr.sin_addr.s_addr =inet_addr(ipAddress);
+    	bzero(&(their_addr.sin_zero), 8);
+	
+	if(connect(sockfd,(struct sockaddr*)&their_addr,sizeof(struct sockaddr))==-1)
+	{
+	    perror("connect");
+	    exit(1);
+	}//socket connect
+	else
+	{
+            printf("connect ok!\n");
+        }
 }
+
 CTelnet::~CTelnet()
 {
-	
+	close(sockfd);  //close socket 
 }
--- multi_DUT_Codes/R6300/TestPerf_R6300/code/Telnet.h
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/Telnet.h
@@ -1,7 +1,25 @@
+#include <stdio.h>	
+#include <stdlib.h> 
+#include <errno.h>	
+#include <string.h> 
+#include <sys/time.h>	
+#include <netdb.h>	
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 class CTelnet
 {
-	
+public:
+	fd_set rfds;
+	int sockfd;
+    struct sockaddr_in their_addr;
+    char sendData[300];
+    char ipAddress[20];
+
+		
 public:
 	CTelnet(char *ipAddr);
 	~CTelnet();
