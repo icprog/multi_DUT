9349d94afba92806dd4371d6fae4dfe97ab9f2f3
zfh1005 <zfh1005@126.com>
2015-04-25 16:43:29 +0800

    complete the AmbtParser file
    complete the AmbtParser file

--- multi_DUT_Codes/R6300/TestPerf_R6300/code/AmbtParser.cpp
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/AmbtParser.cpp
@@ -1,2 +1,111 @@
-#include "AmbtParser.h"
+#include "AmbitParser.h"
+
+/*this function is copy some character from string to other case one spec character was happen*/
+int CopyCaseStr(char *pDst, int DstLen, const char *pSrc, char caseCH)
+{
+
+	const char *pTok=0;
+
+	if ((!pDst)||(!pSrc))
+	{
+		return 0;
+	}
+
+	pTok=strchr(pSrc, caseCH);
+	if (!pTok)
+	{
+		*pDst=0;
+		return 0;
+	}
+
+	char *pstr=strncpy(pDst,pSrc, (pTok-pSrc));
+	if(pstr)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+/*parser the parameter from data*/
+int ParserGetPara(char *pDesBUFF, const char *pTagBuff, char *pRtnConBUFF, int RtnConBUFFLen)
+{
+	char TagBUFF[128] = "";
+	char AnalyseBUFF[512] = "";
+	char ItemID[100]="";
+	char *pToken = NULL;
+	char *pFlagToken = NULL;
+	char *pConStart = NULL;
+	char *PConEnd = NULL;
+
+	if((NULL==pDesBUFF)||(NULL==pTagBuff)||(NULL==pRtnConBUFF))
+	{
+		return 0;
+	}
+	if ((RtnConBUFFLen <= 0) || (RtnConBUFFLen > 512))
+	{
+		return 0;
+	}
+	
+	
+	strcpy(AnalyseBUFF,pDesBUFF);
+	strcpy(TagBUFF,pTagBuff);
+
+	pFlagToken = NULL;
+	pFlagToken=strstr(AnalyseBUFF,TagBUFF);
+	if (!pFlagToken)
+	{
+        return 0;
+	}
+
+	if (!CopyCaseStr(ItemID, sizeof(ItemID),pFlagToken,'='))
+	{
+		strcpy(pRtnConBUFF,"");
+		return 0;
+	}
+
+	if (strlen(ItemID)!=strlen(TagBUFF))
+	{
+		strcpy(pRtnConBUFF,"");
+		return 0;
+	}
+
+
+	if (!strncmp(ItemID, TagBUFF, strlen(TagBUFF)))
+	{
+		pToken = strstr(pFlagToken, "="); 
+		if (pToken)
+		{
+			pConStart = pToken;
+			pConStart += 1;  
+		}
+		else
+		{
+			return 0;
+		}
+		pToken = strstr(pToken, ";"); 
+		if (pToken)
+		{
+			PConEnd = pToken;
+			*PConEnd = 0;
+		}
+		else
+		{
+			return 0;
+		}
+		if (pConStart && PConEnd)
+		{
+			strcpy(pRtnConBUFF,pConStart);
+		}	
+		return 1;
+	}
+	else
+	{
+		strcpy(pRtnConBUFF,"");
+		return 0;
+	}
+
+}
 
--- multi_DUT_Codes/R6300/TestPerf_R6300/code/AmbtParser.h
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/AmbtParser.h
@@ -1,5 +1,14 @@
 #ifndef AMBIT_PARSER_H
 #define AMBIT_PARSER_H
+#include "AmbitTestSysDef.h"
+
+#define FILE_PASER_BUF_L 512
+
+/*parser the parameter from data*/
+int ParserGetPara(char *pDesBUFF, const char *pTagBuff, char *pRtnConBUFF, int RtnConBUFFLen);
+
+/*this function is copy some character from string to other case one spec character was happen*/
+int CopyCaseStr(char *pDst, int DstLen, const char *pSrc, char caseCH);
 
 #endif
 
