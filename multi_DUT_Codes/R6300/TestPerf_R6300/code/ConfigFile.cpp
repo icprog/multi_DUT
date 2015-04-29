8c3a25f638cad9ddc3f17e70efee2ff5cf11b7ca
zfh1005 <zfh1005@126.com>
2015-04-29 17:56:41 +0800

    add some function into CConfigFile function
    add some function into CConfigFile function.

--- multi_DUT_Codes/R6300/TestPerf_R6300/code/ConfigFile.cpp
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/ConfigFile.cpp
@@ -186,3 +186,354 @@ int CConfigFile::LegalJudge()
 	}
 	return 1;
 }
+
+
+int CConfigFile::FindPaketSEflag(char *PBuf, size_t DataLen)
+{
+
+	int Index=0;
+	int FindTarget=0;
+	int insertIndex=0;
+
+	for (Index=0;Index<(int)DataLen;Index++)
+	{
+		if (PACKET_START == *(PBuf+Index))
+		{
+			StNeDFlag[insertIndex].StFlagIndex=Index;
+			FindTarget=1;
+
+			if (!(Index<(int)DataLen))
+			{
+				break;
+			}
+		}
+
+		if(PACKET_END == *(PBuf+Index))
+		{
+
+			StNeDFlag[insertIndex++].EdFlagIndex=Index;
+
+			FindTarget=2;
+		}
+	}
+	if (1==FindTarget)
+	{
+		insertIndex++;
+	}
+	else if (0==FindTarget)
+	{
+		StNeDFlag[insertIndex].EdFlagIndex=-3;
+		StNeDFlag[insertIndex++].StFlagIndex=-3;
+	}
+	StNeDFlag[insertIndex].EdFlagIndex=StNeDFlag[insertIndex].StFlagIndex=-2;
+	return FindTarget;
+}
+
+
+int CConfigFile::InsertHashMap(char *pDesBUFF, char *pTagBuff, char *pRtnConBUFF, int  RtnConBUFFLen)
+{
+
+	char TagBUFF[128] = "";
+	char AnalyseBUFF[1024] = "";
+	char ItemID[100]="";
+	char *pToken = NULL;
+	char *pFlagToken = NULL;
+	char *pConStart = NULL;
+	char *PConEnd = NULL;
+
+	if((NULL==pDesBUFF)||(NULL==pTagBuff)||(NULL==pRtnConBUFF))
+	{
+		return 0;
+	}
+	if ((RtnConBUFFLen <= 0) || (RtnConBUFFLen > 512))
+	{
+		return 0;
+	}
+
+
+
+	strcpy(AnalyseBUFF,pDesBUFF);
+	char *token=strtok(AnalyseBUFF, ";");
+	while( token != NULL )
+	{
+		token = strtok( NULL, ";" );
+	}
+
+	return 0;
+
+	strcpy(AnalyseBUFF,pDesBUFF);
+	strcpy(TagBUFF,pTagBuff);
+
+
+	if (!CopyCaseStr(ItemID, sizeof(ItemID),pFlagToken,'='))
+	{
+		strcpy(pRtnConBUFF, "");
+		return 0;
+	}
+
+	if (strlen(ItemID)!=strlen(TagBUFF))
+	{
+		strcpy(pRtnConBUFF,"");
+		return 0;
+	}
+
+
+
+	if (!strncmp(ItemID, TagBUFF, strlen(TagBUFF)))
+	{
+		pToken = strstr(pFlagToken, "=");
+		if (pToken)
+		{
+			pConStart = pToken;
+			pConStart += 1;
+		}
+		else
+		{
+			return 0;
+		}
+		pToken = strstr(pToken, ";");
+		if (pToken)
+		{
+			PConEnd = pToken;
+			*PConEnd = 0;
+		}
+		else
+		{
+			return 0;
+		}
+		if (pConStart && PConEnd)
+		{
+			strcpy(pRtnConBUFF,pConStart);
+		}
+		return 1;
+	}
+	else
+	{
+		strcpy(pRtnConBUFF, "");
+		return 0;
+	}
+}
+
+/*this function is copy some character from string to other case one spec character was happen*/
+int CConfigFile::CopyCaseStr(char *pDst, int DstLen, const char *pSrc, char caseCH)
+{
+
+	const char *pTok=0;
+
+	if ((!pDst)||(!pSrc))
+	{
+		return 0;
+	}
+
+	pTok=strchr(pSrc, caseCH);
+	if (!pTok)
+	{
+		*pDst=0;
+		return 0;
+	}
+	strncpy(pDst,pSrc, (pTok-pSrc));
+	return 1;
+}
+
+int CConfigFile::RemoveBeforeChrStr(char *pDst, int DstLen, const char *pSrc, char caseCH)
+{
+
+	const char *pTok=0;
+
+	if ((!pDst)||(!pSrc))
+	{
+		return 0;
+	}
+
+	pTok=strrchr(pSrc, caseCH);
+	if (!pTok)
+	{
+		return(!(strcpy(pDst,pSrc)));
+	}
+
+	return(!(strcpy(pDst,pTok+1)));
+
+}
+
+int CConfigFile::CopyCaseAfterStr(char *pDst, int DstLen, const char *pSrc, char caseCH)
+{
+
+	const char *pTok=0;
+
+	if ((!pDst)||(!pSrc))
+	{
+		return 0;
+	}
+
+	pTok=strchr(pSrc, caseCH);
+	if (!pTok)
+	{
+		*pDst=0;
+		return 0;
+	}
+
+	return(!(strcpy(pDst,pTok+1)));
+}
+
+unsigned char CConfigFile::HEXConvert (unsigned char ucHEX)
+{
+	unsigned char A = 'A', a='a',zero = '0';
+
+	if (ucHEX >= a) return (ucHEX-a+10);
+	if (ucHEX >= A) return (ucHEX-A+10);
+	if (ucHEX >= zero) return (ucHEX-zero);
+
+	return 0;
+}
+
+int CConfigFile::StrHEXConvert(char *psrc, int iCovertNum,unsigned int *pRtn)
+{
+
+	int i = 0;
+	int j = 0;
+	unsigned int  dwResult=0;
+	unsigned  char ByteTemp1 = 0;
+	unsigned  char ByteTemp2 = 0;
+	unsigned  char ByteTemp = 0;
+	char GetBuf[100] = "";
+	char BufTemp[100] = "";
+
+	char *pTok=NULL;
+
+	if (!psrc)
+	{
+		return 0;
+	}
+
+	strcpy(BufTemp,psrc);
+
+	pTok=strchr(BufTemp, 'x');
+	if (pTok)
+	{
+		strcpy(GetBuf,(pTok+1));
+	}
+	else
+	{
+		return 0;
+	}
+
+	for (i = 0,j = 0; i < iCovertNum; i+=2, j++)
+	{
+		ByteTemp1 = HEXConvert (GetBuf[i]);
+		ByteTemp2 = HEXConvert (GetBuf[i+1]);
+		ByteTemp = ByteTemp1 << 4 | ByteTemp2;
+		dwResult|=ByteTemp;
+		if (i>=(iCovertNum-2))
+		{
+			break;
+		}
+		dwResult <<= 8;
+	}
+	*pRtn=dwResult;
+	return 1;
+}
+
+char *CConfigFile::HexToStrConvert(char *PtrRtn, unsigned int  uiHex)//convert a ui to char
+{
+
+	int iIndex = 0;
+	int iMaskIndex = 0;
+	int iMask = 0xFF;
+
+	if (!PtrRtn)
+	{
+		return 0;
+	}
+
+	for (iIndex = 0,  iMaskIndex = 3; iIndex < 4; iIndex++, iMaskIndex--)
+	{
+		*(PtrRtn + iIndex) = (uiHex & (iMask<<(8 * iMaskIndex)))>>(8 * iMaskIndex);
+	}
+	*(PtrRtn + iIndex) = 0;
+	return PtrRtn;
+
+}
+
+
+int CConfigFile::ParserGetPara(char *pDesBUFF, const char *pTagBuff, char *pRtnConBUFF, int  RtnConBUFFLen)
+{
+
+	char TagBUFF[128] = "";
+	char AnalyseBUFF[1024] = "";
+	char ItemID[100]="";
+	char *pToken = NULL;
+	char *pFlagToken = NULL;
+	char *pConStart = NULL;
+	char *PConEnd = NULL;
+
+	if((NULL==pDesBUFF)||(NULL==pTagBuff)||(NULL==pRtnConBUFF))
+	{
+		return 0;
+	}
+	if ((RtnConBUFFLen <= 0) || (RtnConBUFFLen > 512))
+	{
+		return 0;
+	}
+
+
+	strcpy(AnalyseBUFF,pDesBUFF);
+	strcpy(TagBUFF,pTagBuff);
+
+	pFlagToken = NULL;
+	pFlagToken=strstr(AnalyseBUFF,TagBUFF);
+	if (!pFlagToken)
+	{
+		return 0;
+	}
+
+
+	if (!CopyCaseStr(ItemID, sizeof(ItemID),pFlagToken,'='))
+	{
+		strcpy(pRtnConBUFF,"");
+		return 0;
+	}
+
+	if (strlen(ItemID)!=strlen(TagBUFF))
+	{
+		strcpy(pRtnConBUFF, "");
+		return 0;
+	}
+
+
+
+	if (!strncmp(ItemID, TagBUFF, strlen(TagBUFF)))
+	{
+		pToken = strstr(pFlagToken, "=");
+		if (pToken)
+		{
+			pConStart = pToken;
+			pConStart += 1;
+		}
+		else
+		{
+			return 0;
+		}
+		pToken = strstr(pToken, ";");
+		if (pToken)
+		{
+			PConEnd = pToken;
+			*PConEnd = 0;
+		}
+		else
+		{
+			return 0;
+		}
+		if (pConStart && PConEnd)
+		{
+			strcpy(pRtnConBUFF,pConStart);
+		}
+		return 1;
+	}
+	else
+	{
+		strcpy(pRtnConBUFF, "");
+		return 0;
+	}
+
+}
+}
