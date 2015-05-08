6c49aeca111aa6ae4a17465815384ddea719345d
zfh1005 <zfh1005@126.com>
2015-05-08 18:12:37 +0800

    add some function into CSocketConnection class
    add some function into CSocketConnection class.

--- multi_DUT_Codes/R6300/TestPerf_R6300/code/SocketConnection.cpp
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/SocketConnection.cpp
@@ -1,10 +1,108 @@
- #include "SocketConnection.h"
+#include "SocketConnection.h"
  
+#include <pthread.h>
+#include <semaphore.h>
+
+int ConnectionIf;
+CSocketConnection *psd;
+extern int TestEnd;
+extern bool gThreadExitFlag;
+extern pthread_t TestThreadID;
+
 CSocketConnection::CSocketConnection()
 {
-	
+	int ret;
+	ret=sem_init(&hRevListMutex,0,1);
 }
+
 CSocketConnection::~CSocketConnection()
 {
 
+}
+
+unsigned long CSocketConnection:: recive_from(char *pData, unsigned long size)
+{         
+        int selectRet = revSocket.select_self();
+        if(selectRet>0)
+        {
+        	int  bytesRecv=revSocket.recvmsg(pData,size);
+        	printf("return select\n");
+        	return bytesRecv;
+        }
+        else if(selectRet==0)
+        {
+        	printf("return no select\n");
+        	return -1;
+        	
+        }
+        else
+        	return -2;
+
+	
+}
+
+int CSocketConnection::InsertRevList(DOT_SOC_BUF *p)
+{
+	sem_wait(&hRevListMutex);
+	RevList.push_back(*p);
+	sem_post(&hRevListMutex);
+	return 1;
+}
+bool CSocketConnection::socketSend(char* sendbuffer)
+{
+	int duration=0;
+    struct timeval tvStart,tvEnd;
+	ensureSymbol=0;
+	bool sendIf= psd->revSocket.sendmsg(sendbuffer,strlen(sendbuffer));
+	gettimeofday(&tvStart,NULL);
+	if(!sendIf)
+	{
+		return false;
+	}
+	
+	while((ensureSymbol==0)&&(duration<10))
+	{
+		usleep(1);
+		gettimeofday(&tvEnd,NULL);
+	    duration=tvEnd.tv_sec-tvStart.tv_sec;
+	}
+    if(duration>=10)
+    {
+        ensureSymbol=1;
+        
+    }
+    
+	return true;
+}
+bool CSocketConnection::Connection()
+{
+	SocketSer.create();
+	SocketSer.bind(10000);
+	pthread_t ConnectionID;
+
+	if(!pthread_create(&ConnectionID,NULL,(void* (*) (void*))MainConnectionThread,this))
+	    return true;
+	else
+	    return false;
+}
+int CSocketConnection::GetRevList(char *p, int size)
+{
+	DOT_SOC_BUF msgtemp;
+
+	sem_wait(&hRevListMutex);
+	if (!RevList.empty())
+	{
+		msgtemp=RevList.front();
+		RevList.pop_front();
+		sem_post(&hRevListMutex);
+	}
+	else
+	{
+		sem_post(&hRevListMutex);
+		return 0;
+	}
+
+	memcpy((void*)p, (void*)&msgtemp.Data, size);
+	printf("msgtemp.Len: %d\n",(int)(msgtemp.Len));
+	return msgtemp.Len;
 }
--- multi_DUT_Codes/R6300/TestPerf_R6300/code/SocketConnection.h
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/SocketConnection.h
@@ -1,12 +1,42 @@
 // Definition of the Socket server
 #ifndef SOCKET_CONNECTION_H
 #define SOCKET_CONNECTION_H
+#include"Socket.h"
+#include<iostream>
+#include <semaphore.h>
+#include<string>
+#include<list>
+
+using namespace std;
+	typedef struct DotSocBuffTag
+{
+	unsigned long Len;
+	char Data[3000];
+}DOT_SOC_BUF;
+
 
 class CSocketConnection
 {
 public:
-  CSocketConnection();
-  virtual ~CSocketConnection();
+	CSocketConnection();
+	virtual ~CSocketConnection();
+
+public:
+	bool Connection();
+	bool socketSend(char* sendbuffer);
+	int GetRevList(char *p, int size);
+	unsigned long recive_from(char *pData, unsigned long size);
+	int InsertRevList(DOT_SOC_BUF *p);
+
+public:
+	Socket revSocket,SocketSer;;
+	int m_sock;
+	int ensureSymbol;
+	sockaddr_in m_addr;
+	sem_t hRevListMutex;
+	list<DOT_SOC_BUF> RevList;
+	
+
 };
 
 #endif
