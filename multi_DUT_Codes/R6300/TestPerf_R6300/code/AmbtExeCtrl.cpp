68844893c28bc09e92ba483d20e262d5b24eedc1
zfh1005 <zfh1005@126.com>
2015-05-12 17:22:59 +0800

    add more function into the AmbtExeCtrl
    add more function into the AmbtExeCtrl.
    Add some function used for check the data format.

--- multi_DUT_Codes/R6300/TestPerf_R6300/code/AmbtExeCtrl.cpp
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/AmbtExeCtrl.cpp
@@ -204,3 +204,161 @@ int EnsureConfigResult(TEST_ITEM *pTI,const char *cmd,const char *spec,int flag)
 	else
 		return 0;
 }
+
+/*
+	check the Muti config file format is right.
+*/
+int EnsureMutiConfigResult(TEST_ITEM *pTI,const char *cmd,const char *spec)
+{       
+    char result[5200],log[1024],SpecValue[100],*specItem=NULL,*returnItem=NULL,*specPoint=NULL;
+    memset(result,0,sizeof(result));
+    memset(SpecValue,0,sizeof(SpecValue));
+    memset(log,0,sizeof(log));
+    int value=RunExeFile(pTI,result,cmd);
+    if(value==3)
+    	return 3;
+    if(strncmp(result,"received:",strlen("received:")))
+    {
+    	if(strstr(result,"\n"))
+    	{
+        	strncpy(log,result,strstr(result,"\n")-result);
+        	pTI->Para.ModifyHashMapItem("ERR_DES_ADD",log);
+        }
+        else
+        {
+           pTI->Para.ModifyHashMapItem("ERR_DES_ADD",result); 
+        }
+        amprintf("Error message:%s",result);
+    	return 2;
+    }
+    specPoint = pTI->Para.GetHashMapStrPara(spec);
+    if(!specPoint)
+    {
+    	char specStr[20]= "";
+		sprintf(specStr,"No \"%s\"",spec);
+		pTI->Para.ModifyHashMapItem("ERR_DES_ADD",specStr);
+    	return 3;
+    }
+	strncpy(SpecValue,specPoint,strlen(specPoint));
+	amprintf("value in Config=%s\n",SpecValue);
+	if(!strncmp(result+10,strtok(SpecValue,"/"),strlen(strtok(SpecValue,"/"))))
+	{
+		returnItem=result;
+		while(specItem)
+		{
+		
+			returnItem=strstr(returnItem,"\n\n");
+			specItem=strtok(NULL,"/");
+			if(!specItem)
+			    return 0;	
+			if(strncmp(returnItem+12,specItem,strlen(specItem)))
+			{
+				return 0;
+			}	
+		}
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+/*
+	check test result.
+*/
+int EnsureResult(TEST_ITEM *pTI,const char *RunResult,const char *cmd)
+{
+	char result[5200];
+	char log[1024];
+	memset(log,0,sizeof(log));
+    memset(result,0,sizeof(result));
+	int value=RunExeFile(pTI,result,cmd);
+    if(value==3)
+    	return 3;
+    if(strncmp(result,"received:",strlen("received:")))
+    {
+    	if(strstr(result,"\n"))
+    	{
+        	strncpy(log,result,strstr(result,"\n")-result);
+        	pTI->Para.ModifyHashMapItem("ERR_DES_ADD",log);
+        }
+        else
+        {
+           pTI->Para.ModifyHashMapItem("ERR_DES_ADD",result); 
+        }
+	    amprintf("Error message:%s",result);
+    	return 2; 
+    }
+	if(!strncmp(result+10,RunResult,strlen(RunResult)))
+		return 1;
+	else
+		return 0;
+	
+		
+}
+
+/*
+	check the SFIF result format right ?
+*/
+int EnsureSFISResult(TEST_ITEM *pTI,const char *cmd,const char *SFISItem,int flag)
+{
+	char result[5200],log[1024],SFISValue[100],*sfis;
+    memset(result,0,sizeof(result));
+    memset(SFISValue,0,sizeof(SFISValue));
+    memset(log,0,sizeof(log));
+           
+    int value=RunExeFile(pTI,result,cmd);
+    
+   if(value==3)
+    	return 3;
+    if(strncmp(result,"received:",strlen("received:")))
+    {
+    	if(strstr(result,"\n"))
+    	{
+        	strncpy(log,result,strstr(result,"\n")-result);
+        	pTI->Para.ModifyHashMapItem("ERR_DES_ADD",log);
+        }
+        else
+        {
+           pTI->Para.ModifyHashMapItem("ERR_DES_ADD",result); 
+        }
+	    amprintf("Error message:%s",result);
+    	return 2;
+    }
+    sfis=SFISDataHashMap.GetHashMapStrPara(SFISItem);
+	if(sfis==NULL)
+	{
+		char sfisStr[20]= "";
+		sprintf(sfisStr,"No \"%s\" SFIS item",SFISItem);
+		pTI->Para.ModifyHashMapItem("ERR_DES_ADD",sfisStr);
+		amprintf("Error:%s",sfisStr);
+		return 2;
+	}
+	else
+	{
+	    strncpy(SFISValue,sfis,strlen(sfis));
+	}
+	amprintf("Value in SFIS=%s\n",SFISValue);
+	if(flag == LOG_TO_MYDAS)
+	{
+    	memset(pTI->Result.Result,0,sizeof(pTI->Result.Result));
+        char result1[5200];
+        memset(result1,0,sizeof(result1));
+        strcpy(result1,result);
+        char *result1End = NULL;
+        result1End = strstr(result1+10,"\n");
+        if(result1End)
+        {
+            *result1End = '\0';
+        }
+        strcpy(pTI->Result.Result,result1+10);
+       	memset(pTI->Result.ResultAim,0,sizeof(pTI->Result.ResultAim));
+        strncpy(pTI->Result.ResultAim,SFISValue,strlen(SFISValue));
+     }
+	if(!strncmp(result+10,SFISValue,strlen(SFISValue)))
+		return 1;
+	else
+		return 0;	
+}
+
--- multi_DUT_Codes/R6300/TestPerf_R6300/code/AmbtExeCtrl.h
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/AmbtExeCtrl.h
@@ -16,4 +16,14 @@ int RunCommand(TEST_ITEM *pTI,char *result);
 /*check the config file format right.*/
 int EnsureConfigResult(TEST_ITEM *pTI,const char *cmd,const char *spec,int flag);
 
+/*check the Muti config file format is right.*/
+int EnsureMutiConfigResult(TEST_ITEM *pTI,const char *cmd,const char *spec);
+
+/*check test result.*/
+int EnsureResult(TEST_ITEM *pTI,const char *RunResult,const char *cmd);
+
+/*check the SFIF result format right ?*/
+int EnsureSFISResult(TEST_ITEM *pTI,const char *cmd,const char *SFISItem,int flag);
+
+
  
