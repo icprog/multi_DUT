b40933669cb27b6c38a88f5d484e7311761bf342
zfh1005 <zfh1005@126.com>
2015-05-13 15:31:48 +0800

    add function into VersionAndLog
    add function into VersionAndLog.
    The function used for print result and program version.

--- multi_DUT_Codes/R6300/TestPerf_R6300/code/VersionAndLog.cpp
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/VersionAndLog.cpp
@@ -1,3 +1,62 @@
 #include "VersionAndLog.h"
+#include <stdio.h>
+
+
+/*
+	print the result 
+*/
+void PrintPassFail(bool bPassFail)
+{
+    if(!bPassFail)
+    {
+		printf("\r\n");
+		printf("========================================================================\n");
+        printf("FFFFFFFFFF     AA        IIIIII    LL          EEEEEEEEEE  DDDDDD     !!\r\n");
+		printf("FFFFFFFFFF   AA  AA      IIIIII    LL          EEEEEEEEEE  DDDDDD     !!\r\n");
+		printf("FF          AA    AA       II      LL          EE          DD   DD    !!\r\n");
+		printf("FF          AA    AA       II      LL          EE          DD    DD   !!\r\n");
+		printf("FFFFFF      AAAAAAAA       II      LL          EEEEEE      DD    DDD  !!\r\n");
+		printf("FFFFFF      AAAAAAAA       II      LL          EEEEEE      DD    DDD  !!\r\n");
+		printf("FF          AA    AA       II      LL          EE          DD    DD   !!\r\n");
+		printf("FF          AA    AA     IIIIII    LLLLLLLLLL  EEEEEEEEEE  DDDDDD     !!\r\n");
+		printf("FF          AA    AA     IIIIII    LLLLLLLLLL  EEEEEEEEEE  DDDDDD     !!\r\n");
+		printf("========================================================================\n");
+		printf("Board is not functioning properly.\r\n");
+    }
+    else
+    {
+        printf("\r\n");
+		printf("========================================================================\n");
+        printf("PPPPPPPPP      AA      SSSSSSSSSS   SSSSSSSSS  EEEEEEEEEE  DDDDDD     !!\r\n");
+		printf("PPPPPPPPPP   AA  AA   SSSSSSSSSSS  SSSSSSSSSS  EEEEEEEEEE  DDDDDD     !!\r\n");
+		printf("PP      PP  AA    AA  SS           SS          EE          DD   DD    !!\r\n");
+		printf("PP      PP  AA    AA  SS           SS          EE          DD    DD   !!\r\n");
+		printf("PPPPPPPPP   AAAAAAAA   SSSSSSSSS    SSSSSSSS   EEEEEE      DD    DDD  !!\r\n");
+		printf("PPPPPPPP    AAAAAAAA   SSSSSSSSS    SSSSSSSS   EEEEEE      DD    DDD  !!\r\n");
+		printf("PP          AA    AA           SS          SS  EE          DD    DD   !!\r\n");
+		printf("PP          AA    AA           SS          SS  EE          DD   DD    !!\r\n");
+		printf("PP          AA    AA  SSSSSSSSSSS  SSSSSSSSSS  EEEEEEEEEE  DDDDDD     !!\r\n");
+		printf("PP          AA    AA  SSSSSSSSSS   SSSSSSSSS   EEEEEEEEEE  DDDDDD     !!\r\n");
+		printf("========================================================================\n");
+		printf("### Board passed all tests!!! ###\r\n");
+    }
+
+}
+
+/*
+	print the program version
+*/
+void PrintTheVersion()
+{
+
+   printf("*******************************************************************************\n");
+   printf("\n");
+   printf("               Test program, version:%s .\n", SOFTWARE_VERSION);
+   printf("               Copyright(C) Ambt Micro system.   \n");
+   printf("               PE II RD Software Team     \n");
+   printf("\n");
+   printf("*******************************************************************************\n");
+
+}
 
 
--- multi_DUT_Codes/R6300/TestPerf_R6300/code/VersionAndLog.h
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/VersionAndLog.h
@@ -2,6 +2,19 @@
 #define VERSION_AND_LOG_H
 
 
+#include "AmbtTestSysDef.h"
+
+
+#define PATH_BUF_SIZE 256
+#define SROM_BUF_SIZE 4096
+#define MAX_MESSAGE_LENGTH      1024
+
+/*print the result */
+void PrintPassFail(bool bPassFail);
+
+/*print the program version*/
+void PrintTheVersion();
+
 #endif
 
 
