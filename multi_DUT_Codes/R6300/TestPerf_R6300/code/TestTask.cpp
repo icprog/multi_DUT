afcf6a40b20f6d0939416f3ae4c5b7f52bf62b89
zfh1005 <zfh1005@126.com>
2015-05-28 18:11:08 +0800

    add some function into project
    add some function into program.

--- multi_DUT_Codes/R6300/TestPerf_R6300/code/TestTask.cpp
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/TestTask.cpp
@@ -3,3 +3,73 @@
  */
 #include "TestTask.h"
 
+
+/*
+	Compare the max length.
+*/
+int strMaxLenCmp(const char *p1, const char *p2)
+{
+    size_t len;
+    len=strlen(p1);
+    if (strlen(p2)>len)
+    {
+        len=strlen(p2);
+    }
+    return (strncmp(p1,p2,len));
+}
+
+
+/*
+	prepare the test.
+*/
+int PrepareTestWork()
+{
+
+    list<TEST_ITEM>::iterator Cy;
+    for (Cy=TItemList.begin(); Cy!=TItemList.end();Cy++)
+    {           
+        Cy->Para.ModifyHashMapItem("ERR_DES_ADD",(char *)"None");
+    }
+    return 1;
+}
+
+
+/*
+	Initial the test 
+*/
+int InitialTestWork()
+{
+
+    list<TEST_ITEM>::iterator Cy;
+    for (Cy=TItemList.begin(); Cy!=TItemList.end();Cy++)
+    {           
+        if (TFLAG_INI_RUN!=Cy->Para.GetHashMapIntPara(ITEM_ENABLE_FALG))//judge the test flag
+        {
+            continue;
+        }
+
+        ItemTestTask(&(*Cy));
+    }
+    return 1;
+}
+
+/*
+	After the test 
+*/
+int AfterTestWork()
+{
+
+    list<TEST_ITEM>::iterator Cy;
+    for (Cy=TItemList.begin(); Cy!=TItemList.end();Cy++)
+    {                   
+        if (TFLAG_TERMINATE_RUN!=Cy->Para.GetHashMapIntPara(ITEM_ENABLE_FALG))//judge the test flag
+        {
+            continue;
+        }
+
+        ItemTest(&(*Cy));
+    }
+    return 1;
+}
+
+
--- multi_DUT_Codes/R6300/TestPerf_R6300/code/TestTask.h
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/TestTask.h
@@ -2,6 +2,16 @@
 #ifndef TETS_TASK_H
 #define TETS_TASK_H
 
+/*Compare the max length.*/
+int strMaxLenCmp(const char *p1, const char *p2);
 
+/*prepare the test.*/
+int PrepareTestWork();
+
+/*Initial the test */
+int InitialTestWork();
+
+/*After the test */
+int AfterTestWork();
 #endif
 
