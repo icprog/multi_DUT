e164cfe9d0894e347da1fbcfd0f253ce49b4086b
zfh1005 <zfh1005@126.com>
2015-06-05 17:57:27 +0800

    add some function into project.
    add some function into project.

--- multi_DUT_Codes/R6300/TestPerf_R6300/code/ControlLogic.cpp
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/ControlLogic.cpp
@@ -13,3 +13,83 @@ CControlLogic::CControlLogic(void)
 CControlLogic::~CControlLogic(void)
 {
 }
+
+/*
+	init the CControlLogic class
+*/
+int CControlLogic::Init()
+{
+
+	if (!UIConnection())//establish connection UI between testperf
+	{
+		printf("ERROR Can not connect server.exit\n");
+		return 0;
+	}
+	if (!ReadConfigFile())
+	{
+
+		return 0;
+	}
+
+	if (!StartAMBTUIParseCammandTask())
+	{
+		return 0;
+	}
+    if(!StartAutoFixtureTask())
+    {
+        return 0;
+    }
+
+	usleep(1);
+	return 1;
+}
+
+/*
+	Read config file.
+*/
+int CControlLogic::ReadConfigFile(void)
+{
+
+	CConfigFile ConfigFile;
+	if(ConfigFile.GetLoadConfigFile((char *)"downFile.txt"))
+	    return (ConfigFile.PerformParse((char *)"ambtconfig.txt"));
+	else
+	    return 0;
+}
+
+/*
+	test information cross
+*/
+int CControlLogic::InformationCross(void)
+{
+	SendIDInfoToUI();
+	return 0;
+}
+
+/*
+	parse the test command
+*/
+int CControlLogic::ParseCommand(char *pCMD,int DataLen)
+{
+	printf("Recv CMD:%s\n", pCMD);
+	int ret=AcceptCMD(pCMD,DataLen);
+	printf("ACCEPT OK\n");
+	return ret;
+	//return 0;
+}
+
+/*
+	connect the UI program .
+*/
+int CControlLogic::UIConnection()
+{
+	std::cout<<"Server open.\n";
+
+	if(SocketConnection.Connection())
+	{
+		sleep(10);
+		return 1;
+	}
+	else
+	return 0;
+}
--- multi_DUT_Codes/R6300/TestPerf_R6300/code/ControlLogic.h
+++ multi_DUT_Codes/R6300/TestPerf_R6300/code/ControlLogic.h
@@ -7,6 +7,26 @@ public:
 	CControlLogic();
 public:
 	~CControlLogic();
+public:
+	/*init the CControlLogic class*/
+    int Init();
+	
+	/*Read config file.*/
+	int ReadConfigFile(void);
+	
+	/*test information cross*/
+	int InformationCross(void);
+	
+	/*parse the test command*/
+	int ParseCommand(char * pCMD,int DataLen);
+	
+	/*connect the UI program .*/
+    int UIConnection();
+	
+public:
+	pthread_t dwParsePktThreadID;
+	pthread_t dwAutoFixThreadID; 
+};
 
 };
 #endif
