f31c2a6d1fc6b00a20ec4a69d6baa3bf646c6bf7
zfh1005 <zfh1005@126.com>
2015-06-15 14:28:08 +0800

    add compareString.py into project.
    add compareString.py into project.
    It's used for compare the two strings and print the different items.

---
+++ multi_DUT_Codes/R6300/TestPerf_R6300/Tools/compareString.py
@@ -0,0 +1,40 @@
+'''
+This is a python file, the python version is v2.7.10
+
+it's used for compare the two string.  
+'''
+
+temp1 = "111 222 5555"
+temp2 = "aaa bbb ccc 111"
+
+temp_1 = temp1.strip().split(' ')
+temp_2 = temp2.strip().split(' ')
+
+result_1 = list()
+result_2 = list()
+
+'''
+If in string_1's items not in string _2, print it.
+'''
+for i in range(0, len(temp_1)):
+    if temp_2.count(temp_1[i]) == 0 :
+        result_1.append(temp_1[i])
+
+print(result_1)
+
+'''
+If in string_2's items not in string _1, print it.
+'''
+for j in range(0, len(temp_2)):
+    if temp_1.count(temp_2[j]) == 0 :
+        result_2.append(temp_2[i])        
+print(result_2)
+
+
+
+
+
+
+
+
+
