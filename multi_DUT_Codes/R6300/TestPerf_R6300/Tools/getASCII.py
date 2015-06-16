eaec0babeb7946a5a9554b3c13b1d9d86119146c
zfh1005 <zfh1005@126.com>
2015-06-16 10:37:50 +0800

    add getASCII.py into project.
    add getASCII.py into project.
    This used for Converent the MAC string to the ASCII and change to base
    16 rate.

---
+++ multi_DUT_Codes/R6300/TestPerf_R6300/Tools/getASCII.py
@@ -0,0 +1,16 @@
+'''
+Converent the MAC string to the ASCII and change to base 16 rate.
+'''
+
+import sys
+
+def getASCII(MAC):
+    result = ''
+    for i in range(len(MAC)):        
+        temp = hex(ord(MAC[i].lower()))
+        result += temp.strip().split('0x')[-1]
+    return result
+
+print(getASCII('ab122222:2222'))
+
+
