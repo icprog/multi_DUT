7c53e0ddd48a40140d43425b8321469c79772287
zfh1005 <zfh1005@126.com>
2015-06-17 16:21:07 +0800

    change the file to a runable file
    change the file to a runable file.

--- multi_DUT_Codes/R6300/TestPerf_R6300/Tools/compareFolder.py
+++ multi_DUT_Codes/R6300/TestPerf_R6300/Tools/compareFolder.py
@@ -10,7 +10,15 @@ import os
 import listTheFile
 import compareString
 
-result_1 = listTheFile.ListFilesToTxt(r'c:\users')
-result_2 = listTheFile.ListFilesToTxt(r'c:\FFOutput')
+PATH_1 = r'c:\users'
+PATH_2 = r'c:\FFOutput'
 
-compareString.compareString(result_1, result_2)
+if __name__ == '__main__':
+    if len(sys.argv) < 2 :
+        result_1 = listTheFile.ListFilesToTxt(PATH_1)
+        result_2 = listTheFile.ListFilesToTxt(PATH_2)
+    else:
+        result_1 = listTheFile.ListFilesToTxt(sys.argv[1])
+        result_2 = listTheFile.ListFilesToTxt(sys.argv[2])
+
+    compareString.compareString(result_1, result_2)
