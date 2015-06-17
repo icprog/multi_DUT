f0b826bba99f4e3413c11c718eafbb3629fcc870
zfh1005 <zfh1005@126.com>
2015-06-17 10:17:19 +0800

    change the file to reasonable struct.
    change the file to reasonable struct.

--- multi_DUT_Codes/R6300/TestPerf_R6300/Tools/listTheFile.py
+++ multi_DUT_Codes/R6300/TestPerf_R6300/Tools/listTheFile.py
@@ -6,23 +6,33 @@ list the file path file
 
 import sys
 import os
+import traceback
 
-def ListFilesToTxt(dir1,file1):
+OUTFILE = r"c:\result.txt" 
+
+def ListFilesToTxt(dir1):
+    result = list()
     for filename in os.listdir(dir1):
-        file1.write(filename + "\n")
+        writeFile(OUTFILE, filename)
+        result.append(filename)
+    return result
 
 def Test():
     dir1 = r"C:\Windows"
-    outfile = r"c:\result.txt"    
-   
-    file1 = open(outfile,"w")
-    if not file1:
-        print ("cannot open the file %s for writing" % outfile)
-    ListFilesToTxt(dir1,file1)
-   
-    file1.close()
-
-
+    result = ListFilesToTxt(dir1)
+    print(result)
+ 
+def writeFile(f, d):
+    try:
+        f = open(f, 'w')
+        f.write(d)
+        if '\n' not in d:
+            f.write('\n')
+        f.flush()
+        f.close()
+    except:
+        traceback.print_exc()
+        
 Test()
 
 
